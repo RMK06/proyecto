dfae54dacd642ed5ad0d51da711b1e7ce27cf940
Carlos <cevivas7@misena.edu.co>
2022-10-29 17:59:01 -0500

    errores agregar.php

--- proyecto_sena/php/agregar.php
+++ proyecto_sena/php/agregar.php
@@ -7,13 +7,13 @@
     <form class="col s11 l11">
       <div class="row">
         <div class="input-field col s6 l6">
-        	<i class="material-icons prefix">account_circle</i>
-          <input id="first_name" type="text" class="validate">
+        	<em class="material-icons prefix">account_circle</em>
+          <emnput id="first_name" type="text" class="validate">
           <label for="first_name">Nombres</label>
         </div>
         <div class="input-field col s6 l6">
-          <i class="material-icons prefix">account_circle</i>
-          <input id="last_name" type="text" class="validate">
+          <em class="material-icons prefix">account_circle</em>
+          <emnput id="last_name" type="text" class="validate">
           <label for="last_name">Apellidos</label>
         </div>
       </div>
@@ -26,22 +26,22 @@
   </div>
      <div class="row">
         <div class="input-field col s12 l6">
-        	 <i class="material-icons prefix">badge</i>
-          <input id="password" type="number" class="validate">
+        	 <em class="material-icons prefix">badge</em>
+          <emnput id="password" type="number" class="validate">
           <label for="password">No. Cedula</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12 l6">
-        	<i class="material-icons prefix">account_circle</i>
-        	 <i class="material-icons prefix">badge</i>
-          <input id="password" type="password" class="validate">
+        	<em class="material-icons prefix">account_circle</em>
+        	 <em class="material-icons prefix">badge</em>
+          <emnput id="password" type="password" class="validate">
           <label for="password">Password</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="email" type="email" class="validate">
+          <emnput id="email" type="email" class="validate">
           <label for="email">Email</label>
         </div>
       </div>
@@ -49,7 +49,7 @@
         <div class="col s12">
           This is an inline input field:
           <div class="input-field inline">
-            <input id="email_inline" type="email" class="validate">
+            <emnput id="email_inline" type="email" class="validate">
             <label for="email_inline">Email</label>
             <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
           </div>
