8d79aba355ba3724d42d5864d1515ee5476b9a32
Carlos <cevivas7@misena.edu.co>
2022-10-29 10:36:28 -0500

    proyecto

--- consultas/colaboradores.php
+++ consultas/colaboradores.php
@@ -1,33 +1,36 @@
 <?php 
 
-	function todos_colaboradores() {
-		require_once ('conexion.php');
+	function todosColaboradores()
+	{
+		require_once('conexion.php');
 		$sql = "SELECT * FROM `usuarios` WHERE `acceso` = 0 ";
-		$result = $conn->query($sql); 
+		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
 			$array = array();
 			while ($row = $result->fetch_assoc() ) {
 				array_push($array, $row);
 			} return $array;
-		} 
+		}
 		$conn->close();
 	}
 
-	function tColaboradores() {
-		require_once ('conexion.php');
+	function tColaboradores()
+	{
+		require_once('conexion.php');
 		$sql = "SELECT * FROM `usuarios` ";
-		$result = $conn->query($sql); 
+		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
 			$array = array();
-			while ($row = $result->fetch_assoc() ) {
+			while ($row = $result->fetch_assoc())
+			{
 				array_push($array, $row);
 			} return $array;
-		} 
+		}
 		$conn->close();
 	}
 
 	function cargo_por_id($a) {
-		require_once ('conexion.php');
+		require_once('conexion.php');
 		$sql = "SELECT * FROM `cargos` WHERE `id` = $a ";
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
@@ -41,4 +44,3 @@
 	function usuarioId() {
 		echo 123;
 	}
-?>
--- consultas/permisos.php
+++ consultas/permisos.php
@@ -1,13 +1,15 @@
 <?php
-    function consultarPermisos($a) {
-		require_once ('conexion.php');
+    function consultarPermisos($a)
+	{
+		require_once('conexion.php');
 		$sql = "SELECT * FROM `permisos` WHERE `id_usuario` = '".$a."' ";
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
 			$array = array();
 			while ($row = $result->fetch_assoc()) {
-				array_push($array,$row);
+				array_push($array, $row);
 			}
 			return $array;
 		}
 	}
+
