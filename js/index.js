d9fef88b4df1a16f04cbf04c9150546898d2dc02
Carlos <cevivas7@misena.edu.co>
2022-10-29 17:12:02 -0500

    errores js

--- js/index.js
+++ js/index.js
@@ -428,14 +428,14 @@ var btn_salir = 0;
 	// Las etiquetas son las que van en el eje X. 
 	var etiquetas = ["Enero", "Febrero", "Marzo", "Abril"]
 	// Podemos tener varios conjuntos de datos. Comencemos con uno
-	var datosVentas2020 = {
+	let datosVentas2020 = {
     	label: "Ventas por mes",
     	data: [5000, 1500, 8000, 5102], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
     	backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
     	borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
     	borderWidth: 1,// Ancho del borde
 	};
-	 new Chart($grafica, {
+	Chart($grafica, {
     	type: 'bar',// Tipo de gráfica
     	data: {
         	labels: etiquetas,
