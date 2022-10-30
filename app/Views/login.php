3667944d56562b681809019898ea65cbb9b50051
Carlos <cevivas7@misena.edu.co>
2022-10-30 10:52:51 -0600

    creando login

---
+++ app/Views/login.php
@@ -0,0 +1,44 @@
+<?= $this->extend('plantilla/layout') ?>
+    <?= $this->section('titulo') ?>
+        prueba
+    <?= $this->endSection() ?>
+    <?= $this->section('contenido') ?>
+        <div class="row">
+            <div class="col s12 l12">
+                <div class="col s6 l6 izquierda"></div>
+                <div class="col s12 l6 panel_der">
+                    <div class="col s12">
+                        <div class="row">
+                            <div class="center logo_login">
+                                <img class="responsive-img img-login" alt=""
+                                    src="almacenamiento/INVENTORY CONTRO_CO.png" draggable="false">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <em class="material-icons prefix">email</em>
+                                <input placeholder="Ej: micorreo@gmail.com" id="correo" type="email"
+                                    class="validate">
+                                <label for="correo">Escriba aquí su correo electrónico:</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <em class="material-icons prefix">lock</em>
+                                <input id="contrasena" type="password" class="validate">
+                                <label for="contrasena">Escriba aquí su contraseña</label>
+                                <span class="helper-text recuperar">¿Olvidó su contraseña?</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="container">
+                                <button class="btn-flat col s12 waves-effect waves-dark iniciar_sesion">
+                                    <em class="material-icons left">exit_to_app</em>Iniciar Sesión
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?= $this->endSection() ?>
