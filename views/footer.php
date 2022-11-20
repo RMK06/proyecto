59c491349d6be241b0f398ff7e3435778011de6e
Carlos <cevivas7@misena.edu.co>
2022-11-20 13:49:44 -0500

    Solucionando novedades del footer

--- views/footer.php
+++ views/footer.php
@@ -3,7 +3,8 @@
         <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        <script src="http://localhost/proyecto/inventory/proyecto/JS/index.js?id=<?php echo date('d-m-Y-h-i-s');?>"></script>
+        <script src="http://localhost/proyecto/inventory/proyecto/JS/index.js?id=<?php echo date('d-m-Y-h-i-s');?>">
+        </script>
         <script type="text/javascript" src="https://unpkg.com/default-passive-events"></script>
         <script type="text/javascript">
                     
