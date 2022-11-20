059da84706630442417a323558d74ff94b6237c0
Carlos <cevivas7@misena.edu.co>
2022-11-20 13:55:23 -0500

    Solucionando novedades de js

--- views/footer.php
+++ views/footer.php
@@ -1,11 +1,20 @@
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        <script src="http://localhost/proyecto/inventory/proyecto/JS/index.js?id=<?php echo date('d-m-Y-h-i-s');?>">
-        </script>
-        <script type="text/javascript" src="https://unpkg.com/default-passive-events"></script>
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js">
+        </script><!-- Sensible -->
+        <script
+            src="https://code.jquery.com/jquery-3.2.1.js">
+        </script><!-- Sensible -->
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
+        </script><!-- Sensible -->
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js">
+        </script><!-- Sensible -->
+        <script
+            src="http://localhost/proyecto/inventory/proyecto/JS/index.js?id=<?php echo date('d-m-Y-h-i-s');?>">
+        </script><!-- Sensible -->
+        <script type="text/javascript" src="https://unpkg.com/default-passive-events"></script><!-- Sensible -->
         <script type="text/javascript">
                     
         </script>
